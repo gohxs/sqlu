79d7b02da02ba97ca718ebda09db2f43ec3d11ab
luis <stdiopt@gmail.com>
2018-04-09 22:02:28 -0300

    Added helper Q()

--- queryer.go
+++ queryer.go
@@ -3,17 +3,40 @@ package sqlu
 import (
 	"context"
 	"database/sql"
+	"errors"
 )
 
-// SQLer wrapper for db, tx
-type Queryer interface {
+// TxQueryer Query only (no transaction initiator)
+type TxQueryer interface {
 	Exec(query string, args ...interface{}) (sql.Result, error)
 	ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
 	Query(query string, args ...interface{}) (*sql.Rows, error)
 	QueryRow(query string, args ...interface{}) *sql.Row
 	QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error)
 }
-type DBer interface {
+
+// Queryer *sql.DB and transaction initiator
+type Queryer interface {
 	Begin() (*sql.Tx, error)
-	Queryer
+	TxQueryer
+}
+
+// Get Queryer
+func Q(db interface{}) Queryer {
+	switch r := db.(type) {
+	case *sql.DB:
+		return r
+	case *sql.Tx:
+		return txWrap{r}
+	default:
+		return nil
+	}
+}
+
+type txWrap struct {
+	TxQueryer
+}
+
+func (txWrap) Begin() (*sql.Tx, error) {
+	return nil, errors.New("not Tx")
 }
